13ece70f312e0e7463da5df5ab0127958cb1e909
LAPTOP-4VOQQ6GI\han <theodorehan373@gmail.com>
2017-03-17 12:26:32 -0400

    Created JPA entity model

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,38 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Plan implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private int id;
+	
+	private String name;
+	
+	public Plan(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
