26705571bfe6822d535e4773fba81ab2fdc3c7b2
LAPTOP-4VOQQ6GI\han <theodorehan373@gmail.com>
2017-03-20 11:56:05 -0400

    Added repositories

--- src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Plan.java
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Plan.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.devopsbuddy.enums.PlansEnum;
+
 @Entity
 public class Plan implements Serializable{
 	
@@ -18,7 +20,12 @@ public class Plan implements Serializable{
 	public Plan(){
 		
 	}
-
+	
+	public Plan(PlansEnum plansEnum) {
+	    this.id = plansEnum.getId();
+	    this.name = plansEnum.getPlanName();
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -34,5 +41,21 @@ public class Plan implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Plan plan = (Plan) o;
+
+        return id == plan.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 	
 }
